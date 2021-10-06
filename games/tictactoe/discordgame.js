d6db70fc472e5da22beea63d6671c44141880826
Jeremy <1cew1r3@gmail.com>
2021-10-05 18:14:24 -0700

    progress on reaction system

--- games/tictactoe/discordgame.js
+++ games/tictactoe/discordgame.js
@@ -1,4 +1,5 @@
 const GameInterface = require('./game');
+const emojiCharacters = require('../../utils/emojiCharacters');
 
 const UNICODE_SQUARE = '□';
 
@@ -7,42 +8,56 @@ class DiscordGame {
     const {bot, msg} = input;
     this.game = new GameInterface();
     this.messageId = null;
+    this.choices = [];
     this.channel = msg.channel;
     this.player = msg.author;
-    this.initBotMessage(this.channel, this.player);
+    this.initBotMessage();
     this.initBotListeners(bot);
   }
 
   initBotListeners(bot) {
     const reactionListener = (reaction, user) => {
+      if (user.bot) return;
       if (reaction.message.id != this.messageId) return;
-      if (user == this.player) {
-        this.channel.send('the right user sent a reaction!');
-      } else {
-        reaction.remove();
+      reaction.remove();
+      if (user != this.player) {
+        return;
+      }
+      if (this.choices.length == 0) {
+        this.choices.push(Object.keys(emojiCharacters)
+            .find((key) => emojiCharacters[key] === reaction.emoji.name));
       }
     // bot.removeListener('messageReactionAdd', reactionListener);
     };
     bot.on('messageReactionAdd', reactionListener);
   }
 
-  async initBotMessage(channel, user) {
+  async initBotMessage() {
     if (this.messageId == null) {
-      this.messageId = await channel.send(
+      this.messageId = await this.channel.send(
           messageContent(this.game));
+      this.channel.messages.fetch(this.messageId)
+          .then(async (message)=> {
+            for (let i = 0; i < this.game.size; i++) {
+              await message.first().react(emojiCharacters[i+1]);
+            }
+          }).catch(console.error);
     }
   }
 }
 
-
 function messageContent(game) {
-  let output = 'Let\'s play!\n```';
+  let output = 'Let\'s play!\n```  ';
+  for (let i = 0; i < game.size; i++) {
+    output += (i+1) + ' ';
+  }
+  output += '\n1 ';
   for (let i = 0; i < game.board.length; i++) {
     output += ((game.board[i] == game.BLANK) ?
               UNICODE_SQUARE :
               game.charSet[game.board[i]]) +' ';
-    if ((i-2) % game.size == 0) {
-      output += '\n';
+    if ((i-2) % game.size == 0 && i != game.board.length-1) {
+      output += '\n' + (2+(i-2)/3)+ ' ';
     }
   }
   return output+'```';
